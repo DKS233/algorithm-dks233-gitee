4d0863e4e78bb4e2385f651ea9b0697951e9eb6a
DKS233 <1137204355@qq.com>
2022-05-20 21:47:53 +0800

    动态规划：象棋（马走日）

---
+++ algorithm-dks233-basic/src/class20/ChessHorse.java
@@ -0,0 +1,86 @@
+package class20;
+
+/**
+ * 请同学们自行搜索或者想象一个象棋的棋盘，
+ * 然后把整个棋盘放入第一象限，棋盘的最左下角是(0,0)位置
+ * 那么整个棋盘就是横坐标上9条线、纵坐标上10条线的区域
+ * 给你三个 参数 x，y，k
+ * 返回"马"从(0,0)位置出发，必须走k步
+ * 最后落在(x,y)上的方法数有多少种?
+ *
+ * @author dks233
+ * @create 2022-05-20-20:13
+ */
+public class ChessHorse {
+    // 暴力递归
+    public static int getMethodOne(int x, int y, int k) {
+        return process(0, 0, x, y, k);
+    }
+
+    // 遍历到(i,j)位置，还剩rest步，最终落在(x,y)上的方法数
+    // x->[0,8] y->[0,9]
+    public static int process(int i, int j, int x, int y, int rest) {
+        if (i > 8 || i < 0 || j < 0 || j > 9) {
+            return 0;
+        }
+        if (rest == 0) {
+            return i == x && j == y ? 1 : 0;
+        }
+        // 不考虑越界有8种走法
+        int count = 0;
+        count += process(i - 1, j + 2, x, y, rest - 1);
+        count += process(i - 2, j + 1, x, y, rest - 1);
+        count += process(i - 1, j - 2, x, y, rest - 1);
+        count += process(i - 2, j - 1, x, y, rest - 1);
+        count += process(i + 1, j + 2, x, y, rest - 1);
+        count += process(i + 2, j + 1, x, y, rest - 1);
+        count += process(i + 1, j - 2, x, y, rest - 1);
+        count += process(i + 2, j - 1, x, y, rest - 1);
+        return count;
+    }
+
+    // 改动态规划
+    // 三个可变参数：i,j,rest
+    // 建三维数组dp
+    // dp[i][j][rest] 到(i,j)位置，还剩rest步，最终落在(x,y)位置的方法数
+    public static int getMethodTwo(int x, int y, int k) {
+        // i [0,8]
+        // j [0,9]
+        // rest [0,k]
+        int[][][] dp = new int[9][10][k + 1];
+        dp[x][y][0] = 1;
+        // 抄暴力递归过程
+        for (int rest = 1; rest <= k; rest++) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 10; j++) {
+                    int count = 0;
+                    count += process(i - 1, j + 2, dp, rest - 1);
+                    count += process(i - 2, j + 1, dp, rest - 1);
+                    count += process(i - 1, j - 2, dp, rest - 1);
+                    count += process(i - 2, j - 1, dp, rest - 1);
+                    count += process(i + 1, j + 2, dp, rest - 1);
+                    count += process(i + 2, j + 1, dp, rest - 1);
+                    count += process(i + 1, j - 2, dp, rest - 1);
+                    count += process(i + 2, j - 1, dp, rest - 1);
+                    dp[i][j][rest] = count;
+                }
+            }
+        }
+        return dp[0][0][k];
+    }
+
+    public static int process(int i, int j, int[][][] dp, int rest) {
+        if (i > 8 || i < 0 || j < 0 || j > 9) {
+            return 0;
+        }
+        return dp[i][j][rest];
+    }
+
+    public static void main(String[] args) {
+        int x = 7;
+        int y = 7;
+        int k = 10;
+        System.out.println(getMethodOne(x, y, k));
+        System.out.println(getMethodTwo(x, y, k));
+    }
+}
