eb75cfea2a0b3909106593c220739d525c45c1a2
DKS233 <1137204355@qq.com>
2022-09-28 15:10:54 +0800

    安天面试题

--- algorithm-dks233-practice/src/other/PrimeCount.java
+++ algorithm-dks233-practice/src/other/PrimeCount.java
@@ -4,29 +4,52 @@ import java.util.Arrays;
 
 /**
  * leetcode204. 计数质数
- * 参考文档：https://leetcode.cn/problems/count-primes/submissions/
+ * 参考文档：https://leetcode.cn/problems/count-primes/solution/kuai-lai-miao-dong-shai-zhi-shu-by-sweetiee/
  *
  * @author dks233
  * @create 2022-08-25-17:53
  */
+@SuppressWarnings("ALL")
 public class PrimeCount {
-    public int countPrimes(int n) {
-        // 先把所有数标记为质数，然后将所有的合数标记为false
-        boolean[] arr = new boolean[n];
-        Arrays.fill(arr, true);
-        for (int i = 2; i * i < n; i++) {
-            if (arr[i]) {
-                for (int j = i * i; j < n; j += i) {
-                    arr[j] = false;
+    // 暴力解法：每个数验证范围：[2,sqrt(number)]
+    public static class MethodOne {
+        public int countPrimes(int n) {
+            int count = 0;
+            for (int i = 2; i < n; i++) {
+                count += process(i) ? 1 : 0;
+            }
+            return count;
+        }
+
+        public boolean process(int number) {
+            for (int i = 2; i * i <= number; i++) {
+                if (number % i == 0) {
+                    return false;
                 }
             }
+            return true;
         }
-        int count = 0;
-        for (int i = 2; i < arr.length; i++) {
-            if (arr[i]) {
-                count++;
+    }
+
+    // 埃氏筛
+    public static class MethdTwo {
+        public int countPrimes(int n) {
+            boolean[] is = new boolean[n];
+            Arrays.fill(is, true);
+            for (int i = 2; i * i < n; i++) {
+                if (is[i]) {
+                    for (int j = i * i; j < n; j += i) {
+                        is[j] = false;
+                    }
+                }
             }
+            int count = 0;
+            for (int i = 2; i < n; i++) {
+                if (is[i]) {
+                    count++;
+                }
+            }
+            return count;
         }
-        return count;
     }
 }
